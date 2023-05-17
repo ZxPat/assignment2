2ed89aa5ec2cd1851f76b8d430b08a3def249585
matteomaglie <matt.mag2002@gmail.com>
2023-05-17 22:21:57 +0200

    Code-Fixes #7

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -9,60 +9,98 @@ import static org.mockito.ArgumentMatchers.anyString;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import org.mockito.internal.matchers.GreaterThan;
+import org.mockito.internal.matchers.LessThan;
+import org.junit.rules.ExpectedException;
 
 public class RomanPrinterTest{
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
     @Test
     public void printTest() throws Exception{
         try(MockedStatic<IntegerToRoman> theMock = Mockito.mockStatic(IntegerToRoman.class)){
-            theMock.when(()-> IntegerToRoman.convert(anyInt()))
-                .thenReturn(anyString( ));
+
+            //prepare
+            String result = "";
+            theMock.when(()-> IntegerToRoman.convert(1))
+                .thenReturn("I");
+            //act
+            result = RomanPrinter.print(1);
+            //assert
+            assertEquals(" _____ \n|_   _|\n  | |  \n  | |  \n _| |_ \n|_____|\n", result);
+
+            //prepare
+            theMock.when(()-> IntegerToRoman.convert(5))
+                .thenReturn("V");
+            //act
+            result = RomanPrinter.print(5);
+            //assert
+            assertEquals("__      __ \n\\ \\    / / \n \\ \\  / /  \n  \\ \\/ /   \n   \\  /    \n    \\/     \n",result);
+            
+            //prepare
+            theMock.when(()-> IntegerToRoman.convert(10))
+                .thenReturn("X");
+            //act
+            result = RomanPrinter.print(10);
+            //assert
+            assertEquals("__   __ \n\\ \\ / / \n \\ V /  \n  > <   \n / . \\  \n/_/ \\_\\ \n",result);
+
+            //prepare
+            theMock.when(()-> IntegerToRoman.convert(50))
+                .thenReturn("L");
+            //act
+            result = RomanPrinter.print(50);
+            //assert
+            assertEquals(" _      \n| |     \n| |     \n| |     \n| |____ \n|______|\n",result);
+
+            //prepare
+            theMock.when(()-> IntegerToRoman.convert(100))
+                .thenReturn("C");
+            //act
+            result = RomanPrinter.print(100);
+            //assert
+            assertEquals("  _____ \n / ____|\n| |     \n| |     \n| |____ \n \\_____|\n",result);
+
+            //prepare
+            theMock.when(()-> IntegerToRoman.convert(500))
+                .thenReturn("D");
+            //act
+            result = RomanPrinter.print(500);
+            //assert
+            assertEquals(" _____  \n|  __ \\ \n| |  | |\n| |  | |\n| |__| |\n|_____/ \n",result);
+
+            //prepare
+            theMock.when(()-> IntegerToRoman.convert(1000))
+                .thenReturn("M");
+            //act
+            result = RomanPrinter.print(1000);
+            //assert
+            assertEquals(" __  __ \n|  \\/  |\n| \\  / |\n| |\\/| |\n| |  | |\n|_|  |_|\n",result);
+
+            //prepare
+            theMock.when(()-> IntegerToRoman.convert(Mockito.intThat(new LessThan<Integer>(1))))
+                .thenThrow(new Exception("Il numero deve essere superiore o uguale a 1"));
+            exception.expect(Exception.class);
+            exception.expectMessage("Il numero deve essere superiore o uguale a 1");
+            //act
+            result = RomanPrinter.print(-5);
+
+            //prepare
+            theMock.when(()-> IntegerToRoman.convert(Mockito.intThat(new GreaterThan<Integer>(3000))))
+                .thenThrow(new Exception("Il codice non assicura la corretta esecuzione con un numero superiore a 3000"));
+
+            exception.expect(Exception.class);
+            exception.expectMessage("Il codice non assicura la corretta esecuzione con un numero superiore a 3000");
+            //act
+            result = RomanPrinter.print(-5);
+
+            
         }
-
-        
-        
-        //prepare
-        String result = "";
-        //act
-        result = RomanPrinter.print(1);
-        //assert
-        assertEquals(" _____ \n|_   _|\n  | |  \n  | |  \n _| |_ \n|_____|\n",result);
-        
-        //act
-        result = RomanPrinter.print(5);
-        //assert
-        assertEquals("__      __ \n\\ \\    / / \n \\ \\  / /  \n  \\ \\/ /   \n   \\  /    \n    \\/     \n",result);
-        
-        //act
-        result = RomanPrinter.print(10);
-        //assert
-        assertEquals("__   __ \n\\ \\ / / \n \\ V /  \n  > <   \n / . \\  \n/_/ \\_\\ \n",result);
-
-        //act
-        result = RomanPrinter.print(50);
-        //assert
-        assertEquals(" _      \n| |     \n| |     \n| |     \n| |____ \n|______|\n",result);
-
-        //act
-        result = RomanPrinter.print(100);
-        //assert
-        assertEquals("  _____ \n / ____|\n| |     \n| |     \n| |____ \n \\_____|\n",result);
-
-        //act
-        result = RomanPrinter.print(500);
-        //assert
-        assertEquals(" _____  \n|  __ \\ \n| |  | |\n| |  | |\n| |__| |\n|_____/ \n",result);
-
-        //act
-        result = RomanPrinter.print(1000);
-        //assert
-        assertEquals(" __  __ \n|  \\/  |\n| \\  / |\n| |\\/| |\n| |  | |\n|_|  |_|\n",result);
-        
-        
-    
-        
     
     }
 }
