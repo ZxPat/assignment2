25b0693c8403a79b1f27ea475d47ca38f4135843
matteomaglie <matt.mag2002@gmail.com>
2023-05-17 22:52:06 +0200

    Code-Fixes #7

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -17,56 +17,94 @@ public class IntegerToRomanTest {
     public ExpectedException exception = ExpectedException.none();
 
     @Test
-    public void convertTest() throws Exception{
-
+    public void convertTest4() throws Exception{
+        //Arrange
         String result="";
         //act
         result = IntegerToRoman.convert(4);
         //Assert
         assertEquals("IV", result);
 
+    }
+
+    @Test
+    public void convertTest5() throws Exception{
+        //Arrange
+        String result="";
         //act
         result = IntegerToRoman.convert(5);
         //Assert
         assertEquals("V", result);
+    }
 
+    @Test
+    public void convertTest9() throws Exception{
+        //Arrange
+        String result="";
         //act
         result = IntegerToRoman.convert(9);
         //Assert
         assertEquals("IX", result);
+    }
 
+    @Test
+    public void convertTest2() throws Exception{
+        //Arrange
+        String result="";
         //act
         result = IntegerToRoman.convert(2);
         //Assert
         assertEquals("II", result);
+    }
 
+    @Test
+    public void convertTest29() throws Exception{
+        //Arrange
+        String result="";
         //act
         result = IntegerToRoman.convert(29);
         //Assert
         assertEquals("XXIX", result);
+    }
 
+    @Test
+    public void convertTest199() throws Exception{
+        //Arrange
+        String result="";
         //act
         result = IntegerToRoman.convert(199);
         //Assert
         assertEquals("CXCIX", result);
+    }
 
+    @Test
+    public void convertTest1900() throws Exception{
+        //Arrange
+        String result="";
         //act
         result = IntegerToRoman.convert(1900);
         //Assert
         assertEquals("MCM", result);
-        
-        //prepare
+    }
+
+    @Test
+    public void convertTestNegative() throws Exception{
+        //Arrange
+        String result="";
         exception.expect(Exception.class);
         exception.expectMessage("Il numero deve essere superiore o uguale a 1");
         //act
         result = IntegerToRoman.convert(-5);
-        
-        //prepare
+    }
+
+    @Test
+    public void convertTestTooLarge() throws Exception{
+        //Arrange
+        String result="";
         exception.expect(Exception.class);
         exception.expectMessage("Il codice non assicura la corretta esecuzione con un numero superiore a 3000");
         //act
         result = IntegerToRoman.convert(3500);
-        
         
     }
 }
